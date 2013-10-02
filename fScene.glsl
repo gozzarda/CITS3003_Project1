c7c64276c30ca4ac62f44d83bd9c1ddf748cb6d0
timothydavies <timothydavies1993@gmail.com>
2013-10-02 12:25:51 +0800

    Implemented G maybe

--- fScene.glsl
+++ fScene.glsl
@@ -1,14 +1,56 @@
 #version 150
 
-in  vec4 color;
 in  vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+in  vec4 position;
+in  vec3 normal;
 
 out vec4 fColor;
 
+vec4 color;
+
 uniform sampler2D texture;
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform mat4 ModelView;
+uniform mat4 Projection;
+uniform vec4 LightPosition;
+uniform float Shininess;
 
 void
 main()
-{
+{    
+	// Transform vertex position into eye coordinates
+    vec3 pos = (ModelView * position).xyz;
+
+
+    // The vector to the light from the vertex    
+    vec3 Lvec = LightPosition.xyz - pos;
+
+    // Unit direction vectors for Blinn-Phong shading calculation
+    vec3 L = normalize( Lvec );   // Direction to the light source
+    vec3 E = normalize( -pos );   // Direction to the eye/camera
+    vec3 H = normalize( L + E );  // Halfway vector
+
+    // Transform vertex normal into eye coordinates (assumes scaling is uniform across dimensions)
+    vec3 N = normalize( (ModelView*vec4(normal, 0.0)).xyz );
+
+    // Compute terms in the illumination equation
+    vec3 ambient = AmbientProduct;
+
+    float Kd = max( dot(L, N), 0.0 );
+    vec3  diffuse = Kd*DiffuseProduct;
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess );
+    vec3  specular = Ks * SpecularProduct;
+    
+    if( dot(L, N) < 0.0 ) {
+	specular = vec3(0.0, 0.0, 0.0);
+    } 
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+    color.rgb = (globalAmbient + ambient + diffuse + specular) / (sqrt(dot(Lvec, Lvec))/15 + 1);	// [GOZ]: Light drops off like 1/(R/15 + 1)
+    color.a = 1.0;
+
+
     fColor = color * texture2D( texture, texCoord * 2.0 );
 }
