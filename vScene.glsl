cde084a809a0f80bb40187699c8a95d769e07a4e
timothydavies <timothydavies1993@gmail.com>
2013-10-28 20:21:16 +0800

    Implemented part A of addingAnimation.txt

--- vScene.glsl
+++ vScene.glsl
@@ -4,6 +4,11 @@ in  vec4 vPosition;
 in  vec3 vNormal;
 in  vec2 vTexCoord;
 
+	//[TFD]: A1 of addingAnimation.txt
+in ivec4 boneIDs;
+in  vec4 boneWeights;
+uniform mat4 boneTransforms[64];
+
 out  vec4 position;
 out  vec3 normal;
 out  vec2 texCoord;
@@ -11,10 +16,22 @@ out  vec2 texCoord;
 uniform mat4 ModelView;
 uniform mat4 Projection;
 
+
 void main()
 {
-	position = vPosition;
-	normal = vNormal;
-    gl_Position = Projection * ModelView * vPosition;
+	//[TFD]: A2 of addingAnimation.txt
+	mat4 boneTransform = boneWeights[0] * boneTransforms[boneIDs[0]];
+
+	for(int i = 1; i < 64; i++) {
+		boneTransform += boneWeights[0] * boneTransforms[boneIDs[0]];
+	}
+
+	//[TFD]: A3: transformed variables used instead of vposition and vnormal
+	vec4 positionTransform = boneTransform * vPosition;
+	vec4 normalTransform = boneTransform * vNormal;
+	
+	position = positionTransform;
+	normal = normalTransform;
+    gl_Position = Projection * ModelView * positionTransform;
     texCoord = vTexCoord;
 }
